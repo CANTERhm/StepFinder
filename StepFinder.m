275f6c03b7a542fef691869456d740d5dfa1941f
julianstar88 <jblaser@hm.edu>
2019-09-11 10:30:37 +0200

    added SmoothData Method

--- StepFinder.m
+++ StepFinder.m
@@ -1,4 +1,4 @@
-classdef StepFinder < afm
+classdef StepFinder
     % STEPFINDER find steps in afm force vs. time curves using the
     % MSF-Algorithm from J. Opfer et al. , 2012 
     %   
@@ -23,10 +23,17 @@ classdef StepFinder < afm
         path % file path to valid afm-txt-curve
         x_data % data-vector of the indipendend variable
         y_data % data-vector of the dependend variable
+        y_conv % smoothed y_data
         sigma % sigma of the gaussian kernel used for data smoothing
         w % half window_width
         N % number of data-points within x/y-data
-    end
+        smoothing_sigma % standard deviation of gaussion smoothing 
+        smoothing_window_width % window withd of gaussian smoothing
+    end % properties
+    
+    properties(Access = private)
+        smoothing_metadata = struct(); % meta-data around data smoothing
+    end % private properties
     
     methods
         function obj = StepFinder(varargin)
@@ -37,8 +44,10 @@ classdef StepFinder < afm
             
             addRequired(p, 'x_data');
             addRequired(p, 'y_data');
-            addParameter(p, 'window_width', 30);
+            addParameter(p, 'window_width', 31);
             addParameter(p, 'sigma', 2);
+            addParameter(p, 'smoothing_sigma', 5);
+            addParameter(p, 'smoothing_window_width',51);
             
             parse(p, varargin{:});
             
@@ -46,6 +55,8 @@ classdef StepFinder < afm
             y_data = p.Results.y_data;
             window_width = p.Results.window_width;
             sigma = p.Results.sigma;
+            smoothing_sigma = p.Results.smoothing_sigma;
+            smoothing_window_width = p.Results.smoothing_window_width;
             
             % call to superclass constructor
             
@@ -55,14 +66,14 @@ classdef StepFinder < afm
                 throw(ME);
             else
                 if length(x_data) ~= length(y_data)
-                    ME = MException('StepFinder:invalidInput', 'the vectors of "x_data" and "y_data" must have the same length');+
+                    ME = MException('StepFinder:invalidInput', 'the vectors of "x_data" and "y_data" must have the same length');
                     throw(ME);
                 end
                 if ~isvector(x_data) || ~isvector(y_data)
                     ME = MException('StepFinder:invalidInput', 'x/y_data inputs have to be double vectors of the size Nx1 or 1xN');
                     throw(ME);
                 else
-                    if ~isnumeric(x_data) || isnumeric(y_data)
+                    if ~isnumeric(x_data) || ~isnumeric(y_data)
                         ME = MException('StepFinder:invalidInput', 'x/y_data have to be numerical vectors');
                         throw(ME);
                     end
@@ -76,19 +87,72 @@ classdef StepFinder < afm
             obj.x_data = x_data;
             obj.y_data = y_data;
             obj.sigma = sigma;
+            obj.smoothing_sigma = smoothing_sigma;
+            obj.smoothing_window_width = smoothing_window_width;
             obj.w = round(window_width/2);
             
         end
         
-        function obj = SmoothData(obj)
+        function obj = SmoothData(obj, varargin)
             % SMOOTHDATA data smoothing by convolution of "y_data" with
-            % a gaussian kernel and the standard deviation "sigma"
+            % a gaussian kernel via the builtin function "smoothdata"
             
+            % input parser
+            p = inputParser;
             
-        end
+            addRequired(p, 'obj');
+            addOptional(p, 'factor', []);
+            
+            parse(p, obj, varargin{:});
+            
+            obj = p.Results.obj;
+            factor = p.Results.factor;
+            
+            % input validation
+            if isempty(factor)
+                s = obj.smoothing_sigma;
+            else
+                s = factor;
+            end
+            
+            % smooth data
+            [kernel, gvec, vec] = gaussian_kernel(obj.smoothing_window_width, s);
+            obj.y_conv = conv(obj.y_data, kernel, 'same');
+            
+            % store smoothing metadata
+            obj.smoothing_metadata.kernel = kernel;
+            obj.smoothing_metadata.x_gauss = vec;
+            obj.smoothing_metadata.y_gauss = gvec;
+            
+        end % SmoothData
+        
+        function obj = PlotData(obj)
+            % yet to be implemented
+        end % PlotData
+        
     end % normal methods
+    
+    methods 
+        
+        function obj = set.smoothing_sigma(obj, sigma)
+            obj.smoothing_sigma = sigma;
+        end % set.smoothing_sigma
+        
+        function obj = set.smoothing_window_width(obj, window_width)
+            obj.smoothing_window_width = window_width;
+        end % set.smoothing_window_withd
+        
+    end % setter/getter methods
     
 end % classdef
 
-function GaussFunction(sigma)
+function varargout = gaussian_kernel(filter_width, sigma)
+    vec = linspace(-(filter_width-1)/2, (filter_width-1)/2, filter_width);
+    gvec = exp(-vec.^2./(2*sigma)^2);
+    kernel = gvec./sum(gvec);
+    
+    varargout{1} = kernel;
+    varargout{2} = gvec;
+    varargout{3} = vec;
+end % gaussain_kernel
 
--- test_convolution_procedure.m
+++ test_convolution_procedure.m
@@ -1,25 +1,23 @@
-curve_data = handles.curveprops.FC_171214M2C1_0245.RawData.CurveData;
-clamp_x = curve_data.Segment5.seriesTime;
+%% curve data
+
+clearvars -except handles main
+
+table = handles.guiprops.Features.edit_curve_table;
+curvename = table.UserData.CurrentCurveName;
+curve_data = handles.curveprops.(curvename).RawData.CurveData;
+clamp_x = curve_data.Segment5.time;
 clamp_y = curve_data.Segment5.vDeflection;
 
+
+%% plot for smoothed data
 fig = figure();
 ax = axes(fig, 'NextPlot', 'add');
 plot(ax, clamp_x, clamp_y, 'DisplayName', 'Clamp-Data');
 grid on
 grid minor
-legend 
+plottools;
 
-% calculate convolution
-% x_gauss = linspace(-1,1,100);
-% % g_vec = gauss(x_gauss,0,0.01);
-% g_vec = normpdf(x_gauss,0,50);
-% y_conv = conv(clamp_y, g_vec, 'same');
-y_conv = smoothdata(clamp_y,'gaussian','SmoothingFactor',0.02);
-
-plot(ax, clamp_x, y_conv, 'r-', 'DisplayName', 'Convolved Data');
-
-
-function g_vec = gauss(x_vec, mu, sig)
-%     g_vec = exp(-(x_vec-mu).^2./(2*sig^2));
-    g_vec = (1/sqrt(2*pi*sig^2)).*exp(-(x_vec-mu).^2./(2*sig^2));
-end
+%% smooth data
+finder = StepFinder(clamp_x, clamp_y);
+finder = finder.SmoothData();
+plot(ax, finder.x_data, finder.y_conv, 'r-', 'DisplayName', 'Smoothded Data');
